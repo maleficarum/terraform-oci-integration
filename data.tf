6a153e6b9c62dcb15f95edf7876ad81120a56daa
Oscar Ivan <o@maleficarum.mx>
2025-04-30 02:03:46 -0600

    Removed IDCS token

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 # tflint-ignore: terraform_unused_declarations
 data "local_file" "idcs_token" {
-  filename = "token.tok"
-  depends_on = [ null_resource.get_idcs_token ]
+  filename   = "token.tok"
+  depends_on = [null_resource.get_idcs_token]
 }
