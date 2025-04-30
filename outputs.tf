a59f7e9f5edba4bf779d3b8e2f74ee35585af7bd
Oscar Ivan <o@maleficarum.mx>
2025-04-30 01:54:08 -0600

    Add:
    - token.tok file as a placeholder to create OIC instance
    - curl execution to get and create the proper token.tok file
    - reading the resulting token.tok in the OCI resource creation

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,11 @@
-output "oic_instance" {
-    value = oci_integration_integration_instance.oic_instance
+/*output "oic_instance" {
+  value       = oci_integration_integration_instance.oic_instance
   description = "The created OIC instance"
+  sensitive   = true
 }
+
+output "data" {
+  value = jsondecode(data.local_file.idcs_token.content)["access_token"]
+  description = "value"
+}
+*/
